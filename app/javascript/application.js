c9b67eceaa7cf59313c517e8293e64105a1e95e8
Muhammad-Ijlal <mijlalckl@gmail.com>
2022-07-29 21:53:50 +0500

    User interface using semantic ui

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,15 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+import jquery from 'jquery';
+window.$ = jquery;
+window.jQuery = jquery;
+
+import "semantic-ui";
+
+$(document).on('turbolinks:load', function() {
+  $('.ui.dropdown')
+    .dropdown()
+  ;
+})
--- config/importmap.rb
+++ config/importmap.rb
@@ -4,4 +4,8 @@ pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+
+
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.6.0/dist/jquery.js", preload: true
+pin "semantic-ui", to: "https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.js"
